02a5564a4d767e560a2490eef9a7f8044ebbd47e
CEOBien <hai.dao.novasquare@gmail.com>
2023-12-11 16:08:01 +0700

    update

--- BackEnd/src/api/models/blog.js
+++ BackEnd/src/api/models/blog.js
@@ -29,7 +29,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
       },
       DESC: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
         allowNull: false,
       },
       IMAGE_PATH: {
